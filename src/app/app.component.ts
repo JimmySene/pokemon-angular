1be7e4882aa8b8f095b892d3ba11a441bba553a3
J.S <jimmy.sene@gmail.com>
2022-08-15 11:30:05 +0200

    select a pokemon with input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,17 @@ import { Pokemon } from './pokemon';
 })
 export class AppComponent implements OnInit {
   pokemons: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemons);
-    this.selectPokemon(this.pokemons[0]);
   }
 
   selectPokemon(pokemon: Pokemon) {
     console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  }
+
+  searchPokemon(pokemonId: string) {
+    this.pokemonSelected = this.pokemons[+pokemonId];
   }
 }
